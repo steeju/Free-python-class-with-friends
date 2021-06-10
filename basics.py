6fd0890ea6ccbb2490cf97464345dccb80d91892
steeju <77688397+steeju@users.noreply.github.com>
2021-06-10 23:16:21 +0700

    Create basics.py
    มาจากที่เรียนฟรีตอนดึกกับ คีน, ปุ๊บปั๊บ

---
+++ basics.py
@@ -0,0 +1,41 @@
+# Output
+print("Hello, world")
+print("13, 12")
+print("World: hello!")
+
+# Variable
+name = "steeju" # String
+age = 19 # Integer
+height = 171.5 # Float
+is_goodboi = True # Bool
+x = 18 < 19 # Bool from statement
+
+print(name)
+print(age)
+print(height)
+
+
+# Operation
+# String
+print("Hello " + "world")
+print("Hello " * 5)
+
+# Int - Float 
+print(18 + 20)
+print(18 - 20)
+print(18 * 20)
+print(5 / 2) # หารทศนิยม
+print(5 // 2) # หารเอาผล
+print(5 % 2) # หารเอาเศษ
+print(2**(1/2)) # ยกกำลัง
+
+# Boolean
+age = 17
+print(not True)
+print((age < 18) or (age > 60))
+print(True or False)
+
+# Input (I/O)
+age = int(input("Input age: "))
+print(type(age))
+print(age)
